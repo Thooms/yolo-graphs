18cd1068333877de2d0d6bb5bc4e2cb9e6531d84
Thomas Papillon <tp@irrealis.me>
2014-10-11 18:30:11 +0200

    The vertices are now also ids-powered

--- src/Vertex.cc
+++ src/Vertex.cc
@@ -1,33 +1,12 @@
 #include "Vertex.hh"
 
-unsigned int Vertex::COUNTER = 0;
-
-void Vertex::counterIncr(void) {
-  ++COUNTER;
-}
-
-void Vertex::counterDecr(void) {
-  --COUNTER;
-}
-
-unsigned int Vertex::getCounter(void) {
-  return COUNTER;
-}
-
-Vertex::Vertex() {
-  id_ = getCounter();
-  counterIncr();
-}
+Vertex::Vertex() {}
 
 Vertex::Vertex(string& name) {
-  Vertex();
-
   setName(name);
 }
 
 Vertex::Vertex(string& name, pair<double, double>& coordinates) {
-  Vertex();
-
   setName(name);
   coordinates_ = coordinates;
 }
--- src/Vertex.hh
+++ src/Vertex.hh
@@ -11,22 +11,10 @@ using namespace std;
 
 class Vertex : public Nameable {
 private:
-  /* COUNTER is the latest available edge id, it is safe to use as
-   * this, but do not forget to increment it afterwards.
-   */
-  static unsigned int COUNTER;
-
   unsigned int id_;
   pair<double, double> coordinates_;
 
 public:
-
-  /* COUNTER behavior */
-
-  static void counterIncr(void);
-  static void counterDecr(void);
-  static unsigned int getCounter(void);
-
   /* Constructors */
 
   explicit Vertex();
