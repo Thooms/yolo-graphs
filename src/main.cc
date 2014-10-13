eb70ef1443624d129d130aa89d2566e141858e0e
Thomas Papillon <tp@irrealis.me>
2014-10-14 01:15:34 +0200

    Modify main to use a test file

--- src/main.cc
+++ src/main.cc
@@ -8,27 +8,18 @@
 #include "EdgeType.hh"
 #include "Vertex.hh"
 #include "DotGraphOutput.hh"
+#include "RawTextGraphInput.hh"
 
 using namespace std;
 
 bool true__(Edge<double>& e) { if (e.distance() >= 0.0) return true; return true; }
 
 int main(void) {
-  DirectedGraph<double> g("France");
+  DirectedGraph<double> g;
 
-  // std::cout << g.name() << std::endl;
+  RawTextGraphInput<double> in(g);
 
-  g.addVertex("Paris", make_pair(50.0, 50.0));
-  g.addVertex("Lyon", make_pair(75.0, 100.0));
-  g.addVertex("Marseille", make_pair(85.0, 130.0));
-  g.addVertex("Lille", make_pair(45.0, 10.0));
-
-  // g.addEdge(EdgeType::Plane, 1000.0, 0, 2); // Paris - Marseille
-  g.addEdge(EdgeType::Road, 500.0, 0, 1); // Paris - Lyon
-  g.addEdge(EdgeType::Road, 350.0, 1, 2); // Lyon - Marseille
-  g.addEdge(EdgeType::Train, 500.0, 0, 1); // Paris - Lyon
-  g.addEdge(EdgeType::Train, 200.0, 0, 3); // Paris - Lille
-
+  in.input("tests/test.yolo");
 
   auto& cu = ComputationUnit<double>::instance();
   cu.setGraph(&g);
