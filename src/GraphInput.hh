93b3a6a4bca2241d5b39c84183d32c2d12ced470
Thomas Papillon <tp@irrealis.me>
2014-10-14 01:14:18 +0200

    Add a GraphInput constructor in header to avoid template undefinability hell

--- src/GraphInput.hh
+++ src/GraphInput.hh
@@ -9,7 +9,7 @@ class GraphInput {
 protected:
   Graph<V>& graph_;
 public:
-  GraphInput(Graph<V>& g);
+  GraphInput(Graph<V>& g) : graph_(g) {}
 
   /* Fills the graph with data. */
   virtual void input(T inputdata) = 0;
