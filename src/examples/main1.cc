db6d0c28168f7c59571806df06adcb779348621e
Flavien Raynaud <raynaudfla@eisti.eu>
2014-10-19 11:26:37 +0000

    Code ready for the first step

--- src/examples/main1.cc
+++ src/examples/main1.cc
@@ -19,24 +19,38 @@ int main(void) {
   UndirectedGraph<double> g;
   RawTextGraphInput<double> in(g);
 
+  // Read the graph from test.yolo
   in << "tests/graphtests/test.yolo";
 
+
+  // Get the computation unit
   auto& cu = ComputationUnit<double>::instance();
   cu.setGraph(&g);
 
-  cout << "BFS from vertex " << g.getVertex(0).name() << endl;
+
+  // City and its neighbors
+  cout << g.getVertex(0).name() << " neighbors: ";
+
+  for (auto vertex : cu.bfs(0, 2, true__))
+  	cout << vertex.name() << " ";
+  cout << endl;
+
+  // City, its neighbors and their neighbors
+  cout << g.getVertex(0).name() << " neighbors, and their neighbors: ";
+
+  for (auto vertex : cu.bfs(0, 3, true__))
+  	cout << vertex.name() << " ";
+  cout << endl;
+
+  // All cities
+  cout << "All cities, starting from " << g.getVertex(0).name() << ": ";
 
   for (auto vertex : cu.bfs(0, 0, true__))
-  	std::cout << vertex.name() << " ";
-  std::cout << std::endl;
-
-  cout << "DFS from vertex " << g.getVertex(0).name() << endl;
-
-  for (auto vertex : cu.dfs(0, 0, true__))
-  	std::cout << vertex.name() << " ";
-  std::cout << std::endl;
+    cout << vertex.name() << " ";
+  cout << endl;
 
 
+  // Output the graph
   DotGraphOutput<double> out(g);
   out >> "truc.dot";
 
