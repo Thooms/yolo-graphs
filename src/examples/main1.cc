ff6ad5b9be30ae1c2fa4b8f31bb572fca653b719
Flavien Raynaud <flavien.raynaud@gmail.com>
2014-12-31 01:30:38 +0100

    Examples are now using maps

--- src/examples/main1.cc
+++ src/examples/main1.cc
@@ -2,62 +2,51 @@
 #include <string>
 #include <utility>
 
-#include "ComputationUnit.hh"
-#include "DirectedGraph.hh"
-#include "UndirectedGraph.hh"
-#include "Edge.hh"
-#include "EdgeType.hh"
-#include "Vertex.hh"
-#include "DotGraphOutput.hh"
-#include "RawTextGraphInput.hh"
-
+#include "DotMapOutput.hh"
 #include "Filter.hh"
+#include "Map.hh"
+#include "RawTextMapInput.hh"
 
 using namespace std;
 
-bool road__(Edge<double>& e) { return e.type() == EdgeType::Road; }
 
 int main(void) {
-  UndirectedGraph<double> g;
-  RawTextGraphInput<double> in(g);
+  Map m;
+  RawTextMapInput in(m);
 
-  // Read the graph from test.yolo
+  // Read the map from test.yolo
   in << "tests/graphtests/test.yolo";
 
-  cout << "Manipulating graph '" << g << "'" << endl;
+  // Base city, arbitrarily chosen for the test
+  City* base = m.getCity(0);
 
-  // Generate filters used later
-  GenericFilter<double>* road_ = new Filter<double>(road__);
-
-  // Get the computation unit
-  auto& cu = ComputationUnit<double>::instance();
-  cu.setGraph(&g);
-
-  Vertex base = g.getVertex(0);
 
   // City and its neighbors
-  cout << base << " neighbors: ";
+  cout << *base << " neighbors: ";
 
-  for (auto vertex : cu.bfs(0, 2, road_))
-  	cout << vertex << " ";
+  for (auto city : m.neighbors(base, NULL))
+    cout << *city << " ";
   cout << endl;
+
 
   // City, its neighbors and their neighbors
-  cout << base << " neighbors, and their neighbors: ";
+  cout << *base << " neighbors, and their neighbors: ";
 
-  for (auto vertex : cu.bfs(0, 3, NULL))
-  	cout << vertex << " ";
+  for (auto city : m.neighborsOfNeighbors(base, NULL))
+    cout << *city << " ";
   cout << endl;
+
 
   // All cities
-  cout << "All cities, starting from " << base << ": ";
+  cout << "All cities, starting from " << *base << ": ";
 
-  for (auto vertex : cu.bfs(0, 0, NULL))
-    cout << vertex << " ";
+  for (auto city : m.allNeighbors(base, NULL))
+    cout << *city << " ";
   cout << endl;
 
-  // Output the graph
-  DotGraphOutput<double> out(g);
+
+  // Output the map
+  DotMapOutput out(m);
   out >> "truc.dot";
 
   return 0;
--- src/examples/main2.cc
+++ src/examples/main2.cc
@@ -2,59 +2,52 @@
 #include <string>
 #include <utility>
 
-#include "ComputationUnit.hh"
-#include "DirectedGraph.hh"
-#include "UndirectedGraph.hh"
-#include "Edge.hh"
-#include "EdgeType.hh"
-#include "Vertex.hh"
-#include "DotGraphOutput.hh"
-#include "RawTextGraphInput.hh"
+#include "DotMapOutput.hh"
+#include "Filter.hh"
+#include "Map.hh"
+#include "RawTextMapInput.hh"
 
 using namespace std;
 
-int main(void) {
-  UndirectedGraph<double> g;
-  RawTextGraphInput<double> in(g);
 
-  // Read the graph from test.yolo
+int main(void) {
+  Map m;
+  RawTextMapInput in(m);
+
+  // Read the map from test.yolo
   in << "tests/graphtests/directed_30_100.yolo";
 
-  cout << "Manipulating graph '" << g << "'" << endl;
+  // Base city, arbitrarily chosen for the test
+  City* base = m.getCity(0);
 
-  
-
-  // Get the computation unit
-  auto& cu = ComputationUnit<double>::instance();
-  cu.setGraph(&g);
-
-  Vertex base = g.getVertex(0);
 
   // City and its neighbors
-  cout << base << " neighbors: ";
+  cout << *base << " neighbors: ";
 
-  for (auto vertex : cu.bfs(0, 2, NULL))
-  	cout << vertex << " ";
+  for (auto city : m.neighbors(base, NULL))
+    cout << *city << " ";
   cout << endl;
+
 
   // City, its neighbors and their neighbors
-  cout << base << " neighbors, and their neighbors: ";
+  cout << *base << " neighbors, and their neighbors: ";
 
-  for (auto vertex : cu.bfs(0, 3, NULL))
-  	cout << vertex << " ";
+  for (auto city : m.neighborsOfNeighbors(base, NULL))
+    cout << *city << " ";
   cout << endl;
+
 
   // All cities
-  cout << "All cities, starting from " << base << ": ";
+  cout << "All cities, starting from " << *base << ": ";
 
-  for (auto vertex : cu.bfs(0, 0, NULL))
-    cout << vertex << " ";
+  for (auto city : m.allNeighbors(base, NULL))
+    cout << *city << " ";
   cout << endl;
 
 
-  // Output the graph
-  DotGraphOutput<double> out(g);
-  out >> "test2.dot";
+  // Output the map
+  DotMapOutput out(m);
+  out >> "truc.dot";
 
   return 0;
 }
